c5153a01f3917cf5948070e6935b6e72c482e11f
EvanLuo42 <knacks.insects_0o@icloud.com>
2023-08-11 13:28:26 +0800

    feat: Danmaku Effect Clear

--- Assets/Tarodev 2D Controller/_Scripts/PlayerController.cs
+++ Assets/Tarodev 2D Controller/_Scripts/PlayerController.cs
@@ -1,12 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Cinemachine;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Serialization;
-using static Unity.VisualScripting.Member;
 
 namespace TarodevController
 {
@@ -69,6 +65,8 @@ namespace TarodevController
             BounceBullet();
 
             DeathChecks();
+            
+            UpdateDanmakuEffects();
 
             if (_canControl) CalculateWalk(); // Horizontal movement
             CalculateAttack();
@@ -600,6 +598,21 @@ namespace TarodevController
 
         #region Danmaku
 
+        private float _effectsPassedTime;
+        private float _effectsDeltaTime;
+        private float _effectsInterval;
+
+        private void UpdateDanmakuEffects()
+        {
+            _effectsDeltaTime += Time.time - _effectsPassedTime;
+            if (_effectsDeltaTime >= _effectsInterval)
+            {
+                _effectsDeltaTime = 0;
+                enableDash = true;
+                _doubleJumpEnabled = true;
+            }
+        }
+
         private void HandleDanmakuEffects(List<Danmaku.DanmakuType> effects)
         {
             foreach (var effect in effects)
@@ -608,12 +621,14 @@ namespace TarodevController
                 {
                     case Danmaku.DanmakuType.DisableDash:
                         enableDash = false;
+                        _effectsDeltaTime = 0;
                         break;
                     case Danmaku.DanmakuType.DashUntilColWall:
                         _consumeDashToWall = true;
                         break;
                     case Danmaku.DanmakuType.DisableDoubleJump:
                         _doubleJumpEnabled = false;
+                        _effectsDeltaTime = 0;
                         break;
                     case Danmaku.DanmakuType.Die:
                         _dead = true;
